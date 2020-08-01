036f98d42277b118e47f224f7fedbbc609fa779e
qiter <9597865@qq.com>
2020-08-01 23:55:04 +0800

    update

--- src/layouts/Basic/container.js
+++ src/layouts/Basic/container.js
@@ -98,9 +98,16 @@ class Container extends React.Component {
                 minHeight: 240,
               }}
             >
-              <Title level={2}>Timesnap</Title>
+              <Title level={2}>图片转场服务</Title>
               <Paragraph>
                 网页转视频功能，可以提供h5转视频服务。转换后可以下载视频，或者png序列帧。
+              </Paragraph>
+              <Paragraph>
+                <ul>
+                  <li>两张图片转场效果</li>
+                  <li>支持网页中带有视频</li>
+                  <li>支持任务中断</li>
+                </ul>
               </Paragraph>
               <Paragraph>
                  依赖库:
