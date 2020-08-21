efdbac19342e1039a43afa4f8946744b099f2302
qiterqi(祁特) <qiterqi@tencent.com>
2020-08-21 18:33:03 +0800

    feat:动态图表排行

--- src/page/comp/picshow/WebD3Chart_bak.js
+++ src/page/comp/picshow/WebD3Chart_bak.js
@@ -37,18 +37,38 @@ class WebD3Chart extends React.Component {
 
   componentDidMount = () => {
     this.drawChartBarHor();
+    // this.drawChartBar();
+
+    // let len = this.state.chartConfig.datas[0].length;
+    // let times = 0;
+    // let durationTime = 1000;
+    // setInterval(() => {
+    //   console.log(times);
+    //   if (times >= len) times = 0;
+    //   times++;
+    // }, durationTime);
+
   };
 
 
   drawChartBarHor = () => {
+
+    // 画布大小
+    let spaceWidth = 500;
+    let spaceHeight = 200;
+    let chartWidth = document.documentElement.clientWidth - spaceWidth;
+    let chartHeight = document.documentElement.clientHeight - spaceHeight;
+
+    window.addEventListener('resize', this.onResize);
+
     const colors = ['#60acfc', '#32d3eb', '#5bc49f', '#feb64d', '#ff7c7c', '#9287e7'];
 
     const chartDatas = [
-      { time: '2008', 北京: 2100, 天津: 5866, 河北: 22986, 山西: 21506, 内蒙古: 34869, 辽宁: 31739 },
-      { time: '2009', 北京: 9600, 天津: 62574, 河北: 24581, 山西: 21522, 内蒙古: 39735, 辽宁: 35149 },
-      { time: '2010', 北京: 7390, 天津: 72994, 河北: 2008, 山西: 26283, 内蒙古: 4747, 辽宁: 42355 },
-      { time: '2011', 北京: 8580, 天津: 85213, 河北: 33969, 山西: 31357, 内蒙古: 57974, 辽宁: 50760 },
-      { time: '2012', 北京: 7475, 天津: 93173, 河北: 36584, 山西: 33628, 内蒙古: 6886, 辽宁: 6409 },
+      { time: '2008', 北京: 21, 天津: 5866, 河北: 22986, 山西: 21506, 内蒙古: 34869, 辽宁: 31739 },
+      { time: '2009', 北京: 96, 天津: 62574, 河北: 24581, 山西: 21522, 内蒙古: 39735, 辽宁: 35149 },
+      { time: '2010', 北京: 739, 天津: 72994, 河北: 28, 山西: 26283, 内蒙古: 4747, 辽宁: 42355 },
+      { time: '2011', 北京: 858, 天津: 85213, 河北: 33969, 山西: 31357, 内蒙古: 57974, 辽宁: 50760 },
+      { time: '2012', 北京: 7475, 天津: 93173, 河北: 36584, 山西: 33628, 内蒙古: 6886, 辽宁: 649 },
       { time: '2013', 北京: 9468, 天津: 100105, 河北: 38909, 山西: 34984, 内蒙古: 67836, 辽宁: 56649 },
       { time: '2014', 北京: 9995, 天津: 105231, 河北: 39984, 山西: 34984, 内蒙古: 71046, 辽宁: 61996 },
       { time: '2015', 北京: 160907, 天津: 107960, 河北: 40255, 山西: 35070, 内蒙古: 71101, 辽宁: 65201 },
@@ -60,7 +80,8 @@ class WebD3Chart extends React.Component {
       { year: 2011, entities: [{ name: "A", value: 100 }, { name: "B", value: 50 }, { name: "C", value: 60 }, { name: "D", value: 20 }, { name: "E", value: 10 }] },
     ];
 
-    let dataAllValues = [];
+    let values = [];
+
 
     const imgList = [
       "http://cross.webdev.com/builder/assets/images/home/showcase/10.png",
@@ -83,130 +104,96 @@ class WebD3Chart extends React.Component {
           o.year = value;
         } else {
           entitiesArr.push({ name: key, name2: `${value}`, value: value, iconUrl: iconUrl });
-          dataAllValues.push(value);
+          values.push(value);
         }
       });
       o.entities = entitiesArr;
       return o;
     });
 
-    let spaceWidth = 500;
-    let spaceHeight = 200;
-    const initWidth = 840;
-    const initHeight = 500;
-
-    const padding = { left: 40, top: 10, right: 20, bottom: 20, space: 20 };
-
-    let height = initHeight - padding.top - padding.bottom;
-    let width = initWidth - padding.left - padding.right;
-
-    const rectBox = { width: 50, height: 50, paddingLeft: 10, paddingTop: 10 };
-    const xPadding = { left: 0 };
-    const yPadding = { bottom: 40 };
-
+    // console.log(JSON.stringify(data))
     const rect = { height: 100, marginV: 10, marginH: 30, marginT: 80 };
     const font = { height: 20, margin: 80 };
-
+    const padding = { left: 40, top: 10, right: 20, bottom: 20 };
+    rect.height = (chartHeight - rect.marginT - font.margin) / data.length;
 
     const svg = d3.select(`#${this.state.chartDomId}`)
       .append("svg")
-      .attr("id", "chart")
-      .attr("width", width)
-      .attr("height", height)
+      .attr("width", chartWidth)
+      .attr("height", chartHeight)
       .style("padding-left", padding.left)
       .style("padding-right", padding.right)
       .style("padding-top", padding.top)
       .style("padding-bottom", padding.bottom);
-
-    // 添加y轴坐标轴
-    let yScale = d3.scaleLinear()
-      .domain([0, d3.max(data)])
-      .range([height - padding.top - padding.bottom - padding.space, 0]);
-
-    // 定义y轴
-    let yAxis = d3.axisLeft(yScale)
-      .tickFormat(d3.format("d")); // 把x,xxx 的数据计数方式格式化，转化为不带逗号的格式
-
-    // 添加x轴坐标轴
-
-    // x轴比例尺
-    let xScale = d3.scaleLinear()
-      .domain([0, d3.max(dataAllValues)])
-      .rangeRound([0, width - padding.left - padding.right]);
-
-    // 定义x轴
-    let xAxis = d3.axisBottom(xScale);
-
-    // 添加x轴
-    const posY = height - padding.top - padding.bottom - yPadding.bottom;
-
-    // 添加
-    // gridlines in x axis function
-    const make_x_gridlines = () => {
-      return d3.axisBottom(xScale);
-    };
-
-
     let durationTime = 1500;
     let animationDurationTime = 700;
     let index = 0;
     let dataEntry = data[index];
-    let entiObj = dataEntry["entities"];
-    let dataValue = entiObj.sort((x, y) => y.value - x.value);
-    let comment = svg.append("text").attr("x", width - 250).attr("y", posY - rectBox.paddingTop)
+    let dataValue = dataEntry["entities"].sort((x, y) => y.value - x.value);
+    // 最大长度，即最后一个矩形的底边 y 值
+    const maxHeight = (rect.marginV + rect.height) * (dataValue.length - 1) + rect.marginT;
+
+    let xScale = d3.scaleLinear().domain([10, d3.max(values) * 1.2]).range([40, (chartWidth * .9)]);
+    // let yScale = d3.scaleLinear().domain([10, d3.max(values) * 1.2]).range([40, maxHeight]);
+
+    let yScale = d3.scaleLinear()
+      .domain([0, d3.max(values)])
+      .range([maxHeight, 0]);
+
+    let _yScale = d3.scaleLinear()
+      .domain([0, d3.max(values)])
+      .range([0, maxHeight]);
+
+    let comment = svg.append("text").attr("x", 0).attr("y", rect.marginT - font.height)
       .attr("fill", "grey").text(`${dataEntry.year}年`).style("font-size", "50");
 
     const groups = svg
       .selectAll("g")
-      .data(dataValue, (d, i) => { }).enter()
+      .data(dataValue).enter()
       .append("g")
-      .attr("class", "group")
       .style("cursor", "pointer")
-      .on("click", () => { });
+      .on("click", () => {
+        // update((++index) % data.length)
+      });
 
     let rects = groups.append("rect")
-      .attr("x", 0)
-      .attr("height", rectBox.height)
+      .attr("x", rect.marginH + font.margin)
+      .attr("height", rect.height)
       .attr("fill", (d, i) => colors[i % colors.length]);
 
     let labels = groups.append("text")
-      .attr("x", rectBox.width)
+      .attr("x", rect.marginH)
       .style("font-size", `${font.height}px`);
 
     let totalLabels = groups.append("text")
-      .attr("x", rectBox.width)
+      .attr("x", rect.marginH + font.margin)
       .style("font-size", `${font.height}px`);
 
     let iconImgs = groups.append("svg:image")
-      .attr("x", 0)
-      .attr("width", rectBox.width)
-      .attr("height", rectBox.height);
+      // .attr("xlink:href", imgUrl)
+      .attr("x", rect.marginH + font.margin)
+      .attr("width", rect.height / 2)
+      .attr("height", rect.height / 2);
 
-    // add the X gridlines
-    const grid = svg.append("g")
-      .attr("id", "grid")
-      .attr("transform", "translate(" + xPadding.left + "," + posY + ")")
-      .call(make_x_gridlines()
-        .tickSize(-posY)
-        .tickFormat("")
-      );
+    // 定义y轴
+    const yAxis = d3.axisLeft(yScale)
+      .tickFormat(d3.format("d")); // 把x,xxx 的数据计数方式格式化，转化为不带逗号的格式
+
     // 添加y轴
-    let axisX = svg.append("g")
+    svg.append("g")
       .attr("class", "axis")
-      .attr("transform", "translate(" + xPadding.left + "," + posY + ")")
-      .call(xAxis);
+      .attr("transform", "translate(" + 0 + "," + 0 + ")")
+      .call(yAxis);
 
-    // let axisY = svg.append("g")
-    //   .attr("class", "axis")
-    //   .attr("transform", "translate(" + xPadding.left + "," + 0 + ")")
-    //   .call(yAxis);
+    // const axisX = svg.append("g")
+    //   .attr("transform", `translate(${font.margin - 10}, ${maxHeight + rect.height + rect.marginV})`)
+    //   .call(d3.axisBottom(xScale));
 
     const resize = window.onresize = () => {
-      width = document.documentElement.clientWidth - spaceWidth;
-      height = document.documentElement.clientHeight - spaceHeight;
-      rectBox.height = height / data.length;
-      svg.attr("width", height).attr("height", height);
-      // xScale = d3.scaleLinear().domain([10, d3.max(values) * 1.2]).range([40, (height * .9)]);
+      chartWidth = document.documentElement.clientWidth - spaceWidth;
+      chartHeight = document.documentElement.clientHeight - spaceHeight;
+      svg.attr("width", chartWidth).attr("height", chartHeight);
+      xScale = d3.scaleLinear().domain([10, d3.max(values) * 1.2]).range([40, (chartWidth * .9)]);
       // axisX.attr("transform", `translate(${font.margin - 10}, ${maxHeight + rect.height + rect.marginV})`)
       // .call(d3.axisBottom(xScale));
     };
@@ -219,39 +206,28 @@ class WebD3Chart extends React.Component {
 
     const updateElements = () => {
 
-      const getPosY = (i) => posY - (rectBox.height + rectBox.paddingTop) * (entiObj.length - i);
-
       rects.data(dataValue, (d, i) => d.name).transition().duration(animationDurationTime)
-        .attr("y", (_, i) => getPosY(i))
-        .attr("transform", `translate(${rectBox.width}, 0)`)
+        .attr("y", (_, i) => (rect.marginV + rect.height) * i + rect.marginT)
         .attr("width", d => xScale(d.value));
 
       labels.data(dataValue, d => d.name).text(d => d.name).transition().duration(animationDurationTime)
-        .attr("y", (_, i) => getPosY(i) + rectBox.height / 2);
+        .attr("y", (_, i) => (rect.marginV + rect.height) * i + rect.marginT + rect.height / 2);
 
       totalLabels.data(dataValue, (d, i) => d.name).text(d => scientificToNumber(d.value)).transition().duration(animationDurationTime)
-        .attr("x", d => xScale(d.value) + rectBox.width + rectBox.paddingLeft)
-        .attr("y", (_, i) => getPosY(i) + rectBox.height / 2);
+        .attr("x", d => xScale(d.value) + rect.marginV + font.margin + 30)
+        .attr("y", (_, i) => (rect.marginV + rect.height) * i + rect.marginT + rect.height / 2);
 
       iconImgs.data(dataValue, (d, i) => d.name).transition().duration(animationDurationTime)
         .attr("xlink:href", d => d.iconUrl)
-        .attr("y", (_, i) => getPosY(i));
+        .attr("y", (_, i) => rect.marginT + (rect.marginV + rect.height) * i);
 
     };
 
-    // resize();
+    resize();
 
-    // updateElements();
+    updateElements();
 
     const update = (i) => {
-
-      dataEntry = data[i];
-      dataValue = dataEntry["entities"].sort((x, y) => y.value - x.value);
-
-      comment.text(`${dataEntry.year}年`);
-
-      updateElements();
-      // clearInterval(intervalId);
 
       index++;
       if (index >= data.length) {
@@ -263,6 +239,13 @@ class WebD3Chart extends React.Component {
         }, 3000);
       }
 
+      dataEntry = data[i];
+      dataValue = dataEntry["entities"].sort((x, y) => y.value - x.value);
+
+      comment.text(`${dataEntry.year}年`);
+
+      updateElements();
+
     };
 
 
@@ -270,10 +253,87 @@ class WebD3Chart extends React.Component {
 
   }
 
-  scientificToNumber = (num) => {
-    const str = num.toString();
-    const reg = str.indexOf(".") > -1 ? /(\d)(?=(\d{3})+\.)/g : /(\d)(?=(?:\d{3})+$)/g;
-    return str.replace(reg, "$1,");
+  drawChartBar = () => {
+    // 画布大小
+    const width = 800;
+    const height = 400;
+
+    // 在 body 里添加一个 SVG 画布    
+    const svg = d3.select(`#${this.state.chartDomId}`)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    // 画布周边的空白
+    const padding = { left: 50, right: 30, top: 20, bottom: 20 };
+    const barWidth = 100;
+
+    // 原始数据
+    const datax = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    let datay = [];
+    for (let i = 1;i <= 7;i++) {
+      datay.push(barWidth * i);
+    }
+    datay = _.shuffle(datay);
+
+    // x轴
+    const xScale = d3.scaleOrdinal()
+      .domain(datax)
+      .range(datay);
+
+    const xAxis = d3.axisBottom()
+      .scale(xScale);
+
+    const translateY = height - padding.bottom;
+
+    svg.append('g')
+      .call(xAxis)
+      .attr("transform", `translate(${0}, ${translateY})`)
+      .selectAll("text")
+      .attr("dx", barWidth / 2);
+
+    // y轴 
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(datay)])
+      .range([height - padding.bottom, padding.top]);
+
+    const yAxis = d3.axisLeft()
+      .scale(yScale)
+      .ticks(10);
+
+    svg.append('g')
+      .call(yAxis)
+      .attr("transform", `translate(${barWidth},0)`);
+
+    const bar = svg.selectAll(".bar")
+      .data(datay)
+      .enter().append("g")
+      .attr("class", "bar")
+      .attr("transform", (d, i) => {
+        // return `translate(${xScale(i * barWidth)}, ${yScale(d)})`;
+        const dx = (i + 1) * (barWidth);
+        const dy = yScale(d);
+        return `translate(${dx}, ${dy})`;
+      });
+
+    const rect = bar.append("rect")
+      .attr("x", 1)
+      .attr("width", barWidth)
+      .attr("height", (d) => { return height - yScale(d) - padding.bottom; })
+      .attr("stroke", "White")
+      .attr("fill", "#f63").on('click', (d, i, obj) => {
+        d3.select(obj[i]).transition().duration(1000)
+          .attr("x", 100)
+          .attr("fill", "yellow");
+      });
+
+    bar.append("text")
+      .attr("dy", ".75em")
+      .attr("y", 6)
+      .attr("x", barWidth / 2)
+      .attr("text-anchor", "middle")
+      .attr("font-size", "18px")
+      .text((d) => d);
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -326,6 +386,7 @@ class WebD3Chart extends React.Component {
 
   render = () => (
     <Wrapper>
+      <div id={this.state.chartDomIdHor}></div>
       <div id={this.state.chartDomId}></div>
       <Wrapper>
         <Button
